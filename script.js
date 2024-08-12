0687522ebff103eecf090bed3cf4b5a8b4d508de
RusHiii... <khakalrushi18@gmail.com>
2024-08-12 22:45:22 +0530

    making recaptcha mandatory

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-console.log("script file working")
 
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("inputdate.value",inputdate.value); //string form
-    let formatedata = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formatedata;
+let captchacheked = false;
+
+function beforesubmit(event){
+    if (captchacheked){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("inputdate.value",inputdate.value); //string form
+        let formatedata = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formatedata;
+    }else {
+        alert("Please checked the reCaptcha box");
+        event.preventDefault();
+    }
+    
 }
 
 function timestamp() { 
@@ -15,4 +22,9 @@ function timestamp() {
         elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     } 
-} setInterval(timestamp, 500); 
+} 
+setInterval(timestamp, 500); 
+
+function capthchasuccess(){
+    captchacheked = true;
+}
